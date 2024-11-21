74efcbda44c6b5b4f09e14c4e2b9296dfb23ef03
artursopelnik <artur.sopelnik93@gmail.com>
2024-11-21 10:08:31 +0100

    fix(setup): add cli-dev-deps; improve styling-setup 🚨

--- packages/lit/src/components/my-component/my-component.ts
+++ packages/lit/src/components/my-component/my-component.ts
@@ -1,7 +1,9 @@
-import { LitElement, css, html } from 'lit'
+import {LitElement, html, CSSResultGroup} from 'lit'
 import { customElement, property } from 'lit/decorators.js'
 import { classMap } from 'lit/directives/class-map.js';
-import {format} from "../../utils/utils";
+import { format } from "../../utils/utils";
+import componentStyles from '../../styles/component.styles'
+import styles from './my-component.styles'
 
 export interface MyComponentProps {
     /** first contents */
@@ -22,6 +24,7 @@ export interface MyComponentProps {
  */
 @customElement('my-component')
 export class MyComponent extends LitElement {
+  static styles: CSSResultGroup = [componentStyles, styles]
 
   @property({ type: String })
   first = ''
@@ -59,52 +62,6 @@ export class MyComponent extends LitElement {
   private _onClick() {
     this.count++
   }
-
-  static styles = css`
-    :host {
-      max-width: 1280px;
-      margin: 0 auto;
-      padding: 2rem;
-      text-align: center;
-    }
-
-    a {
-      font-weight: 500;
-      color: #646cff;
-      text-decoration: inherit;
-    }
-    a:hover {
-      color: #535bf2;
-    }
-
-    button {
-      border-radius: 8px;
-      border: 1px solid transparent;
-      padding: 0.6em 1.2em;
-      font-size: 1em;
-      font-weight: 500;
-      font-family: inherit;
-      background-color: #1a1a1a;
-      cursor: pointer;
-      transition: border-color 0.25s;
-    }
-    button:hover {
-      border-color: #646cff;
-    }
-    button:focus,
-    button:focus-visible {
-      outline: 4px auto -webkit-focus-ring-color;
-    }
-
-    @media (prefers-color-scheme: light) {
-      a:hover {
-        color: #747bff;
-      }
-      button {
-        background-color: #f9f9f9;
-      }
-    }
-  `
 }
 
 declare global {
